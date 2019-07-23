b8d241df762792c4c866e50324ecbed94c6e1464
9910 <webdevmastersensei@gmail.com>
2019-07-23 19:56:41 +0200

    Initial Commit

---
+++ FoodOrderingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodOrderingApp
+//
+//  Created by Marko K on 7/23/19.
+//  Copyright © 2019 Marko K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
